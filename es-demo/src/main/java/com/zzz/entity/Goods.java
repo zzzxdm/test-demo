f50f4479f1ed67aace387b1423a79b37d20ed7b7
zzz <zhuzhenzhenit@163.com>
2019-08-24 21:54:22 +0800

    add es demo

--- es-demo/src/main/java/com/zzz/entity/Goods.java
+++ es-demo/src/main/java/com/zzz/entity/Goods.java
@@ -9,12 +9,14 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.Mapping;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
 @Data
-@Document(indexName = "idx_goos", type = "doc", shards = 1, replicas = 0)
+@Document(indexName = "idx_goods", type = "doc", shards = 1, replicas = 0, createIndex = false)
+@Mapping(mappingPath = "mapping/goods_mapping.json")
 public class Goods {
 
     @Id
@@ -23,7 +25,7 @@ public class Goods {
     @Field(type = FieldType.Text)
     private String name;
 
-    @Field(type = FieldType.Text, analyzer = "ik", searchAnalyzer = "ik")
+    @Field(type = FieldType.Text, analyzer = "ik_max_word", searchAnalyzer = "ik_smart")
     private String desc;
 
     @Field(type = FieldType.Double)
@@ -35,7 +37,7 @@ public class Goods {
     @Field(type = FieldType.Long)
     private Long inventory;
 
-    @DateTimeFormat(pattern = "yyyy-MM-dd\'T\'HH:mm:ss.SSS")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @JsonSerialize(using = LocalDateTimeSerializer.class)
     @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     @Field(type = FieldType.Date)

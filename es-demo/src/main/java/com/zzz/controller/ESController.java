f5658c6755418c1a29d052f58566ec40494c611c
zzz <zhuzhenzhenit@163.com>
2019-08-26 21:37:22 +0800

    optimized rabbitmq demo

--- es-demo/src/main/java/com/zzz/controller/ESController.java
+++ es-demo/src/main/java/com/zzz/controller/ESController.java
@@ -1,8 +1,8 @@
 package com.zzz.controller;
 
-import com.zzz.consumer.utils.RedisUtil;
 import com.zzz.entity.Goods;
 import com.zzz.service.GoodsService;
+import com.zzz.utils.RedisUtil;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
--- es-demo/src/main/java/com/zzz/utils/RedisUtil.java
+++ es-demo/src/main/java/com/zzz/utils/RedisUtil.java
@@ -1,4 +1,4 @@
-package com.zzz.consumer.utils;
+package com.zzz.utils;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;

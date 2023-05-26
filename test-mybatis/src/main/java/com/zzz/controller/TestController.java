db679a663715b913e2aa40401d0d83de253f21d9
zzz <zhuzhenzhenit@163.com>
2023-05-26 15:24:10 +0800

    update

--- test-mybatis/src/main/java/com/zzz/controller/TestController.java
+++ test-mybatis/src/main/java/com/zzz/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.zzz.controller;
 
 import com.zzz.service.CompanyServiceImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
+@Slf4j
 @RestController
 public class TestController {
 
@@ -23,6 +25,13 @@ public class TestController {
         stopWatch.stop();
         long totalTimeMillis = stopWatch.getTotalTimeMillis();
         return "OK " + totalTimeMillis;
+    }
+
+
+    @GetMapping("/testHello")
+    public String testHello() {
+        log.info("Hello Word " + System.currentTimeMillis());
+        return "Hello World";
     }
 
 
